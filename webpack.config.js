0c57f3f838c2a2451184774c73e850b96afbdf7f
fred <fredericnguyen777@gmail.com>
2022-04-25 19:41:55 -0400

    added assets for logo, background image and fonts for the banner of the site. fixed webpack configurations.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,22 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
+  mode: 'development',
+  entry: './src/login.js',
   devtool: 'inline-source-map',
-  devServer : {
-    open: 'Google Chrome',
-    static: './dist'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
      {
        test: /\.(png|svg|jpg|jpeg|gif)$/i,
