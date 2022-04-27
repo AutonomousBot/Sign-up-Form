e2cc3de8f31accef667c63a44b4606cac282c76b
fred <fredericnguyen777@gmail.com>
2022-04-27 19:13:41 -0400

    added logo and adjusted background size. added margins to body (rem). added credits to bg image.

--- src/login.js
+++ src/login.js
@@ -1,2 +1,14 @@
 import './style.css'
+import treeLogo from './assets/treeLogo.png'
 
+function appendTreeImg() {
+  const logoTree = document.getElementById("logo-title");
+  let logo = document.createElement("img")
+  logo.id = "logo"
+  logo.src = treeLogo
+  logo.alt = "Tree logo."
+  const firstChild = logoTree.children[0];
+  logoTree.insertBefore(logo, firstChild)
+}
+
+appendTreeImg();
